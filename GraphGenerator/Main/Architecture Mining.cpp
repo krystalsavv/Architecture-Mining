5710f96e8c73c14d53f41390e077481e7ce3cf27
Krystallia Savvaki <41945729+krystalsavv@users.noreply.github.com>
2021-04-17 19:52:47 +0300

    Command line utility  console messages

--- GraphGenerator/Main/Architecture Mining.cpp
+++ GraphGenerator/Main/Architecture Mining.cpp
@@ -8,12 +8,20 @@
 #include "GraphToJson.h"
 
 static void PrintMainArgInfo(void) {
-	std::cout << "MAIN ARGUMENTS:\n\n";
-	std::cout << "argv[1]: \"--src\" to mine whole directory with sources (argv[2]: directory/with/sources)\n";
-	std::cout << "argv[1]: \"--cmp-db\" to use compilation database (argv[2]: path/to/compile_commands.json)\n";
-	std::cout << "argv[3]: (file path) path/to/ignoredFilePaths\n";
-	std::cout << "argv[4]: (file path) path/to/ignoredNamespaces\n";
-	std::cout << "argv[5]: (file path) path/to/output\n";
+	std::cout << "Usage: Architecture_Miner [options] <files...>\n\n";
+	std::cout << "Options:\n";
+	std::cout << "   --src [directory]:    To analyse a whole directory with sources.\n"; 
+	std::cout << "                         It can only be used if --cmp-db is not used. \n";
+	std::cout << "   --cmp-db [file]:      To analyse a system using a compilation database.\n";
+	std::cout << "                         The file must be named \"compile_commands.json\".\n";
+	std::cout << "                         It can only be used if --src is not used. \n\n";
+	std::cout << "Files (optional):\n";
+	std::cout << "   ignored files:        The first parameter is a text file that defines some\n";
+	std::cout << "                         file paths in order to be exluded from the analysis.\n";
+	std::cout << "   ignored namespaces:   The second parameter is a text file that defines some\n";
+	std::cout << "                         namespaces in order to be exluded from the analysis.\n";
+	std::cout << "   output file:          The last parameter is a JSON file where the generated\n";
+	std::cout << "                         graph will be stored.\n";
 }
 
 int main(int argc, const char** argv) {
@@ -45,33 +53,24 @@ int main(int argc, const char** argv) {
 	std::string fullPath = std::string(__FILE__);
 	std::size_t found = fullPath.find_last_of("/\\");
 	std::string jsonPath = (argc >= 6) ? argv[5] : fullPath.substr(0, found + 1) + "../../GraphVisualizer/Graph/graph.json";
+					
+	std::cout << "\nBeginning Analysis\n\n";
 	
-	/*std::vector<std::string> srcs;
-	srcs.push_back(path + "\\classes_simple.cpp");			
-	srcs.push_back(path + "\\fields.cpp");					
-	srcs.push_back(path + "\\friends.cpp");					
-	srcs.push_back(path + "\\member_classes.cpp");			
-	srcs.push_back(path + "\\methods_args_vars.cpp");		
-	srcs.push_back(path + "\\methods.cpp");					
-	srcs.push_back(path + "\\namespaces.cpp");			
-	//srcs.push_back(path + "\\objects_used_on_methods.cpp");	
-	srcs.push_back(path + "\\template_methods.cpp");		
-	srcs.push_back(path + "\\template_types.cpp");			
-	srcs.push_back(path + "\\templates.cpp");				
-
-	srcs.push_back(path + "\\test0.cpp");					
-	srcs.push_back(path + "\\include.h");						
-	srcs.push_back(path + "\\include2.h");*/
-				
-	std::cout << "\n-------------------------------------------------------------------------------------\n\n";
+	std::cout << "Dependency Extraction... ";
 	int result = dependenciesMining::CreateClangTool(cmpDBPath, srcs, ignoredFilePaths, ignoredNamespaces);
+	std::cout << "Completed\n";
+
+	std::cout << "Dependency Graph Generation... ";
 	graph::Graph graph = graphGeneration::GenetareDependenciesGraph(dependenciesMining::structuresTable);
+	std::cout << "Completed\n";
+
+	std::cout << "JSON Generation... ";
 	std::string json = graphToJson::GetJson(graph);
-
 	std::ofstream jsonFile;
-
 	jsonFile.open(jsonPath);
 	jsonFile << json;
  	jsonFile.close();
-	std::cout << "\n-------------------------------------------------------------------------------------\n\n";
-}
+	std::cout << "Completed\n";
+	
+	std::cout << "\nAnalysis Completed\n\n";
+} 
